cc8a5119b3eb44bbd959b64a8b1bdeb814c05a2f
rgri <robert13grimley@gmail.com>
2023-07-30 12:43:58 -0500

    Updated stuff

--- flake.nix
+++ flake.nix
@@ -16,6 +16,7 @@
     devShells.default = pkgs.mkShell {
       packages = [
         pkgs.nodePackages.npm
+        pkgs.nodePackages.nodejs
         pkgs.nodePackages.rollup
         pkgs.direnv
         pkgs.nodePackages.http-server
