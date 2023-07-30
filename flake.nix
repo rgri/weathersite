52e48a13fd41b0088a9c176e36d9b1fcb2f0750d
rgri <robert13grimley@gmail.com>
2023-07-30 10:17:04 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A very basic flake";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/";
+
+  outputs = { self, nixpkgs, ... }@inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+
+    in {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    devShells.default = pkgs.mkShell {
+      packages = [
+        pkgs.nodePackages.npm
+        pkgs.nodePackages.rollup
+        pkgs.direnv
+      ];
+    };
+
+    });
+}
